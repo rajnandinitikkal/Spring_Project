ce6c6e4fefb7cd26636be9ef3aea8ef238cb7924
rajnandinitikkal <rajnandinitikkal@gmail.com>
2025-04-07 14:12:54 +0530

    add first commit

---
+++ myFirstProject/src/main/java/com/example/myFirstProject/MyFirstProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.myFirstProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstProjectApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		SpringApplication.run(MyFirstProjectApplication.class, args);
+	}
+
+}
